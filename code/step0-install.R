3264e4b28a04dce3be816979e0ce4ab0c3990972
arrowif <95392782@qq.com>
2020-02-14 23:23:54 +0800

    packages needed

--- code/step0-install.R
+++ code/step0-install.R
@@ -1,34 +1,28 @@
-## 
-### ---------------
-###
-### Create: Jianming Zeng
-### Date: 2018-12-20 15:43:52
-### Email: jmzeng1314@163.com
-### Blog: http://www.bio-info-trainee.com/
-### Forum:  http://www.biotrainee.com/thread-1376-1-1.html
-### CAFS/SUSTC/Eli Lilly/University of Macau
-### Update Log: 2018-12-20  First version
-###
-### ---------------
 
 #set mirror for china
 rm(list = ls())   
 options()$repos 
 options()$BioC_mirror
 options(BioC_mirror="https://mirrors.ustc.edu.cn/bioc/")
+options(BioC_mirror="http://bioconductor.org")
 options("repos" = c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/"))
+
+
+
 options()$repos 
 options()$BioC_mirror
 
 
 # https://bioconductor.org/packages/release/bioc/html/GEOquery.html
 if (!requireNamespace("BiocManager", quietly = TRUE))
-      install.packages("BiocManager")
+      install.packages("BiocManager") 
+#install.packages("BiocManager") # sometimes you have to install the latest biomanager first. then you can get the latedst packages
 BiocManager::install("KEGG.db",ask = F,update = F)
 BiocManager::install(c("GSEABase","GSVA","clusterProfiler" ),ask = F,update = F)
 BiocManager::install(c("GEOquery","limma","impute" ),ask = F,update = F)
-BiocManager::install(c("genefu","org.Hs.eg.db","hgu133plus2.db" ),ask = F,update = F)
-
+BiocManager::install("org.Hs.eg.db",ask = F,update = F)
+BiocManager::install("hgu133plus2.db" ,ask = F,update = F)
+BiocManager::install("genefu")
 
 # source("https://bioconductor.org/biocLite.R") 
 # library('BiocInstaller') 
@@ -36,14 +30,14 @@ BiocManager::install(c("genefu","org.Hs.eg.db","hgu133plus2.db" ),ask = F,update
 # BiocInstaller::biocLite("GEOquery")
 # BiocInstaller::biocLite(c("limma"))
 # BiocInstaller::biocLite(c("impute"))
-
+# https://mirrors.ustc.edu.cn/bioc/3.10/data/annotation/src/contrib/  qinghua mirro source site
 options()$repos
 install.packages('WGCNA')
 install.packages(c("FactoMineR", "factoextra"))
 install.packages(c("ggplot2", "pheatmap","ggpubr"))
+
 library("FactoMineR")
 library("factoextra")
-
 library(GSEABase)
 library(GSVA)
 library(clusterProfiler)
@@ -54,3 +48,4 @@ library(hgu133plus2.db)
 library(limma)
 library(org.Hs.eg.db)
 library(pheatmap)
+
